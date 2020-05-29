ff3ca8b0a6ac30a24ed59299bb6750436b5fbc0e
sumit <sk3507788@gmail.com>
2020-05-29 13:55:57 +0530

    add database connection

---
+++ src/helper/javaConnect.java
@@ -0,0 +1,37 @@
+package helper;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sumit Kumar
+ */
+public class javaConnect {
+    
+    public static Connection connectDb()
+    {
+        Connection conn = null;
+        
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:src//ATMManagement.sqlite");
+            
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
+}
